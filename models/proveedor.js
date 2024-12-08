8c4b9594ae611fc59cfb2f42ecc3de54a8a752a2
CristianMosquer <cristiansantoplay2@gmail.com>
2024-12-08 02:30:46 -0500

    1

--- models/proveedor.js
+++ models/proveedor.js
@@ -1,51 +1,57 @@
 import mongoose from "mongoose";
 
 // Esquema para los datos de Proveedor
-const proveedorSchema = new mongoose.Schema({
-  nitProveedor: {
-    type: Number,
-    required: true,
-    unique: true,
+const proveedorSchema = new mongoose.Schema(
+  {
+    nitProveedor: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    contacto: {
+      type: String,
+      required: true,
+    },
+    nombreEmpresa: {
+      type: String,
+      required: true,
+    },
+    direccionEmpresa: {
+      type: String,
+      required: true,
+    },
+    deptoEmpresa: {
+      type: String,
+      required: true,
+    },
+    paisEmpresa: {
+      type: String,
+      required: true,
+    },
+    ciudEmpresa: {
+      type: String,
+      required: true,
+    },
+    telefProveedor: {
+      type: Number,
+      required: true,
+    },
+    emailProveedor: {
+      type: String,
+      required: true,
+    },
+    estadoProveedor: {
+      type: Boolean,
+      default: true,
+    },
   },
-  contacto: {
-    type: String,
-    required: true,
-  },
-  nombreEmpresa: {
-    type: String,
-    required: true,
-  },
-  direccionEmpresa: {
-    type: String,
-    required: true,
-  },
-  deptoEmpresa: {
-    type: String,
-    required: true,
-  },
-  paisEmpresa: {
-    type: String,
-    required: true,
-  },
-  ciudEmpresa: {
-    type: String,
-    required: true,
-  },
-  telefProveedor: {
-    type: Number,
-    required: true,
-  },
-  emailProveedor: {
-    type: String,
-    required: true,
-  },
-  estadoProveedor: {
-    type: Boolean,
-    default: true,
-  },
-}, {
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
-const Proveedor = mongoose.model("Proveedor", proveedorSchema);
+// Verifica si el modelo ya ha sido compilado para evitar recompilaciones
+const Proveedor =
+  mongoose.models.Proveedor || mongoose.model("Proveedor", proveedorSchema);
+
 export default Proveedor;
