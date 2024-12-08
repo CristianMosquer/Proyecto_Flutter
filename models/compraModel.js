009c5134b20e246b8ab0232f7b3bbf68c1789acb
CristianMosquer <cristiansantoplay2@gmail.com>
2024-12-08 01:58:22 -0500

    cb

--- models/compraModel.js
+++ models/compraModel.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-
 const compraSchema = new mongoose.Schema(
   {
     idProveedor: {
--- models/proveedor.js
+++ models/proveedor.js
@@ -1,68 +1,51 @@
-// proveedor.js
 import mongoose from "mongoose";
 
 // Esquema para los datos de Proveedor
-const proveedorSchema = new mongoose.Schema(
-  {
-    // Número de Identificación Tributaria del proveedor
-    nitProveedor: {
-      type: Number,
-      required: true,
-      unique: true,
-    },
-    // Nombre del contacto principal
-    contacto: {
-      type: String,
-      required: true,
-    },
-    // Nombre de la empresa
-    nombreEmpresa: {
-      type: String,
-      required: true,
-    },
-    // Dirección de la empresa
-    direccionEmpresa: {
-      type: String,
-      required: true,
-    },
-    // Departamento donde se encuentra la empresa
-    deptoEmpresa: {
-      type: String,
-      required: true,
-    },
-    // País de la empresa
-    paisEmpresa: {
-      type: String,
-      required: true,
-    },
-    // Ciudad de la empresa
-    ciudEmpresa: {
-      type: String,
-      required: true,
-    },
-    // Teléfono del proveedor
-    telefProveedor: {
-      type: Number,
-      required: true,
-    },
-    // Correo electrónico del proveedor
-    emailProveedor: {
-      type: String,
-      required: true,
-    },
-    // Estado del proveedor (activo/inactivo)
-    estadoProveedor: {
-      type: Boolean,
-      default: true,
-    },
+const proveedorSchema = new mongoose.Schema({
+  nitProveedor: {
+    type: Number,
+    required: true,
+    unique: true,
   },
-  {
-    // Añade automáticamente createdAt y updatedAt
-    timestamps: true,
-  }
-);
+  contacto: {
+    type: String,
+    required: true,
+  },
+  nombreEmpresa: {
+    type: String,
+    required: true,
+  },
+  direccionEmpresa: {
+    type: String,
+    required: true,
+  },
+  deptoEmpresa: {
+    type: String,
+    required: true,
+  },
+  paisEmpresa: {
+    type: String,
+    required: true,
+  },
+  ciudEmpresa: {
+    type: String,
+    required: true,
+  },
+  telefProveedor: {
+    type: Number,
+    required: true,
+  },
+  emailProveedor: {
+    type: String,
+    required: true,
+  },
+  estadoProveedor: {
+    type: Boolean,
+    default: true,
+  },
+}, {
+  timestamps: true,
+});
 
-// Crear el modelo a partir del esquema
 const Proveedor = mongoose.model("Proveedor", proveedorSchema);
-
 export default Proveedor;
